f2260d412109d0915528b483278bbe5a7c1228f2
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2024-01-16 16:52:09 +0000

    Structured the code a bit more and added the functions that actually convert occupancy into event loss probability

--- pps_hitmaps/functions.py
+++ pps_hitmaps/functions.py
@@ -1,4 +1,33 @@
+import ROOT
+from math import exp
 
+def calcEventLossProb(
+        timeStep: float,
+        occupancy: float,
+                      ):
+    return 1 - (occupancy ** 2)/((1 - exp(-occupancy))**2) * exp(-2*occupancy * (timeStep + 1))
 
-def occupancyGraphToLossProbability(occupancy):
-    pass
+def eventLossProbDistrib(
+        timeStep: float,
+        occupancy: list[float],
+                         ):
+    lossProb = []
+
+    for val in occupancy:
+        lossProb += [calcEventLossProb(timeStep, val)]
+
+    return lossProb
+
+def occupancyGraphToEventLossProbability(
+        occupancy: ROOT.TGraph,
+        minTimeStep: int = 0,
+        maxTimeStep: int = 400,
+                                         ):
+    allTimeSteps = range(minTimeStep, maxTimeStep)
+
+    retData = {
+        'length': [],
+        'occupancy': [],
+    }
+
+    return retData
